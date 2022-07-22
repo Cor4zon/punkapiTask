ed7837238df33e96b1eab4cebd1cddfe41dbbf34
Victor Fedorov <vpfedorov@bk.ru>
2022-07-22 14:04:53 +0300

    BeerBoard initial version

---
+++ src/components/BeerBoard.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import BeerCard from "./BeerCard";
+import APIClient from "../services/APIClient";
+
+const BeerBoard = () => {
+    const [beers, setBeers] = useState([]);
+    const client = new APIClient();
+
+    useEffect(() => {
+        client.fetchBeers().then((result) => {
+            setBeers([...result.data]);
+        })
+    }, []);
+
+    const beerBoard = beers.map(beer => {
+            return <BeerCard beer={beer} />
+    });
+
+    return (
+        <div>
+            { beerBoard }
+        </div>
+    );
+};
+
+export default BeerBoard;
