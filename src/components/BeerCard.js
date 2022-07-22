ec85d7b99613ee0728a0955a1f719b146c37ba0d
Victor Fedorov <vpfedorov@bk.ru>
2022-07-22 14:05:03 +0300

    BeerCard initial version

---
+++ src/components/BeerCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from "styled-components";
+
+const ImageBeer= styled.img`
+  width: 100px;
+  height: auto;
+  object-fit: cover;
+`;
+
+const BeerCard = ({ beer }) => {
+    return (
+        <div>
+            <ImageBeer src={beer.image_url} alt={beer.name}/>
+            <h1> { beer.name }</h1>
+            <p> { beer.description }</p>
+        </div>
+    );
+};
+
+export default BeerCard;
