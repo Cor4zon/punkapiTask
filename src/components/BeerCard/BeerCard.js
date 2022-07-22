bf50471a88fbe2f19b8faa3b87aa18e9ac1a2f2e
Victor Fedorov <vpfedorov@bk.ru>
2022-07-22 14:38:21 +0300

    calc correct image size proportion

--- src/components/BeerCard/BeerCard.js
+++ src/components/BeerCard/BeerCard.js
@@ -4,15 +4,17 @@ import styled from "styled-components";
 import "./BeerCard.css";
 
 const ImageBeer= styled.img`
-  width: 100px;
-  height: auto;
+  width: auto;
+  height: 100%;
   object-fit: cover;
 `;
 
 const BeerCard = ({ beer }) => {
     return (
         <div className="beer-card__container">
-            <ImageBeer src={beer.image_url} alt={beer.name}/>
+            <div className="image__container">
+                <ImageBeer src={beer.image_url} alt={beer.name}/>
+            </div>
             <h1> { beer.name }</h1>
             <p> { beer.description }</p>
         </div>
